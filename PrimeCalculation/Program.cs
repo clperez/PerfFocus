240cb5982d6fa4ca9a119267463ca223d4007f48
Carlos L. Perez <carlillos34@gmail.com>
2020-01-21 14:29:16 +0100

    Adding a routine to calculate prime numbers in a naive way

---
+++ PrimeCalculation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace PrimeCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var primeNumber in GetNextPrimeNaive())
+                Console.WriteLine(primeNumber);
+        }
+
+
+        // One core simd 
+        // Multicore paralelization
+        private static IEnumerable<long> GetNextPrimeNaive()
+        {
+            for (var currentValue = 1; ; currentValue++)
+            {
+                bool isPrime = true;
+                for (var currentDenominator = 2; currentDenominator < currentValue; ++ currentDenominator)
+                {
+                    if (!(isPrime = currentValue % currentDenominator != 0)) break;
+                }
+
+                if (isPrime) yield return currentValue;
+            }
+        }
+    }
+
+
+}
